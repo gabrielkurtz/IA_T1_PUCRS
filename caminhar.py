5a67a0295d8f688ac7490f496b7de64bd16746c8
Kurtz <gabriel.kurtz@sap.com>
2019-10-01 14:47:06 -0300

    Improved logs

--- caminhar.py
+++ caminhar.py
@@ -1,13 +1,17 @@
 import random
 import math
 import sys
+import copy
 
-GERACOES = 20
-ELITISMO = 3
-TORNEIO = 3
+LOG_COMPLETO = True
+GERACOES_ENTRE_LOG_SIMPLES = 5
+
+GERACOES = 1000
+ELITISMO = 2
+TORNEIO = 4
 TAM_POPULACAO_INICIAL = ELITISMO + 2*TORNEIO
 FATOR_MAXIMO_PASSOS = 2
-MUTACAO_MINIMA = 2
+MUTACAO_MINIMA = 4
 MUTACAO_MAXIMA = 50
 
 labirinto = []
@@ -18,6 +22,10 @@ contador_cromossomos = 1
 
 alguem_achou_saida = False
 mutacao = MUTACAO_MAXIMA
+
+contador_geracoes = 1
+melhor = None
+
 
 def main():
     f = open(str(sys.argv[1]), "r")
@@ -37,11 +45,23 @@ def main():
     imprime_labirinto()
 
     populacao = inicializa_populacao()
-    
+
     for i in range(GERACOES):
+        global contador_geracoes
+
         executa_geracao(populacao)
+
+        if not LOG_COMPLETO:
+            if contador_geracoes == 1 or (contador_geracoes % GERACOES_ENTRE_LOG_SIMPLES == 0):
+                logar_simples(populacao)
+        contador_geracoes += 1
+
         populacao = nova_geracao(populacao)
 
+    print("----------------------------------------------------------------")
+    print("MELHOR CROMOSSOMO")
+    print("----------------------------------------------------------------")
+    print(melhor)
 
 
 class Cromossomo:
@@ -49,7 +69,7 @@ class Cromossomo:
         global contador_cromossomos
         self.nome = contador_cromossomos
         contador_cromossomos += 1
-        
+
         self.saida = None
         self.encontrou_saida = False
         self.posicao = (0, 0)
@@ -99,15 +119,15 @@ class Cromossomo:
 
     def cria_movimentos(self):
         movimentos = {}
-        movimentos[1] = [1, -1]
-        movimentos[2] = [1, 0]
-        movimentos[3] = [1, 1]
-        movimentos[4] = [0, -1]
-        movimentos[6] = [0, 1]
-        movimentos[7] = [-1, -1]
-        movimentos[8] = [-1, 0]
-        movimentos[9] = [-1, 1]
-        movimentos[0] = [0, 0] # Ocorre somente em caso de erro
+        movimentos[1] = [1, -1, "↙"]
+        movimentos[2] = [1, 0, "↓"]
+        movimentos[3] = [1, 1, "↘"]
+        movimentos[4] = [0, -1, "←"]
+        movimentos[6] = [0, 1, "→"]
+        movimentos[7] = [-1, -1, "↖"]
+        movimentos[8] = [-1, 0, "↑"]
+        movimentos[9] = [-1, 1, "↗"]
+        movimentos[0] = [0, 0, "X"]  # Ocorre somente em caso de erro
         return movimentos
 
     def calcula_nova_posicao(self, direcao):
@@ -117,8 +137,8 @@ class Cromossomo:
     def reinicia_cromossomo(self):
         self.visitados = self.cria_visitados()
         self.passos = []
-        self.posicao = (0,0)
-        self.posicao_anterior = (0,0)
+        self.posicao = (0, 0)
+        self.posicao_anterior = (0, 0)
         self.encontrou_saida = False
         self.percentual_mutacao = mutacao
 
@@ -162,7 +182,7 @@ class Cromossomo:
 
             if 0 <= nova_posicao[0] < tamanho_matriz and 0 <= nova_posicao[1] < tamanho_matriz and labirinto[nova_posicao[0]][nova_posicao[1]] != "1":
                 posicao_valida = True
-            else: 
+            else:
                 count += 1
                 direcao = 0
 
@@ -175,7 +195,6 @@ class Cromossomo:
             self.visitados[self.posicao[0]][self.posicao[1]] = '$'
         if labirinto[self.posicao[0]][self.posicao[1]] == 'S':
             self.encontrar_saida()
-        
 
     def encontrar_saida(self):
         self.saida = list(self.posicao)
@@ -192,13 +211,22 @@ class Cromossomo:
                 len(self.passos) + math.sqrt(2*math.pow(tamanho_matriz, 2))
         return aptidao
 
+    def imprime_setas(self):
+        matriz_direcoes = []
+        for i in range(tamanho_matriz):
+            linha_nova = []
+            for j in range(tamanho_matriz):
+                linha_nova.append(self.movimentos[self.direcoes[i][j]][2])
+            matriz_direcoes.append(linha_nova)
+        return matriz_direcoes
+
     def __str__(self):
         to_string = "----------------------------------------------------------------\n"
-                
+
         to_string += ("Cromossomo " + str(self.nome) + "\n")
         for i in range(tamanho_matriz):
             to_string += str(self.visitados[i]) + \
-                " - " + str(self.direcoes[i]) + "\n"
+                " - " + str(self.imprime_setas()[i]) + "\n"
         to_string += ("Heurística: " + str(self.heuristica()) + " - Posição: " + str(self.posicao) +
                       " - Encontrou: " + str(self.encontrou_saida) + " - Onde: " + str(self.saida) + " - Passos: " + str(len(self.passos)))
         return to_string.replace("'", "").replace(",", "")
@@ -228,6 +256,7 @@ def inicializa_populacao():
         populacao_inicial.append(Cromossomo())
     return populacao_inicial
 
+
 def executa_geracao(populacao):
     global alguem_achou_saida
     alguem_achou_saida = False
@@ -235,8 +264,14 @@ def executa_geracao(populacao):
     for i in range(TAM_POPULACAO_INICIAL):
         populacao[i].executa()
     populacao.sort(key=lambda c: c.heuristica(), reverse=True)
+    global melhor
+    
+    if (melhor == None) or (populacao[0].heuristica() > melhor.heuristica()):
+        melhor = copy.copy(populacao[0])
+    
     imprime_labirinto()
     imprime_populacao(populacao)
+
 
     global mutacao
     if alguem_achou_saida:
@@ -249,21 +284,23 @@ def executa_geracao(populacao):
     if mutacao > MUTACAO_MAXIMA:
         mutacao = MUTACAO_MAXIMA
 
+
 def nova_geracao(populacao):
     p = []
-    
+
     for i in range(ELITISMO):
         p.append(populacao[i])
+        logar("-> Elitismo - Carregando " + str(populacao[i].nome))
 
     for t in range(TORNEIO):
-        print("Torneio " + str(t))
+
         i = random.randint(0, len(populacao)-1)
         c_temp1 = populacao[i]
         i = random.randint(0, len(populacao)-1)
         c_temp2 = populacao[i]
         c1 = c_temp1 if c_temp1.heuristica() > c_temp2.heuristica() else c_temp2
         populacao.remove(c1)
-        
+
         i = random.randint(0, len(populacao)-1)
         c_temp1 = populacao[i]
         i = random.randint(0, len(populacao)-1)
@@ -271,22 +308,39 @@ def nova_geracao(populacao):
         c2 = c_temp1 if c_temp1.heuristica() > c_temp2.heuristica() else c_temp2
         populacao.remove(c2)
 
+        logar("-> Torneio " + str(t) + " - Cruzando: " +
+              str(c1.nome) + " e " + str(c2.nome))
+
         filho_1, filho_2 = cruza_cromossomos(c1, c2)
         p.append(filho_1)
         p.append(filho_2)
-    
+
     return p
 
 
+def logar(s):
+    if LOG_COMPLETO:
+        print(s)
+
+
+def logar_simples(populacao):
+    m = populacao[0]
+    print("-> Melhor Atual - Geracao: " + str(contador_geracoes) + " - Melhor: Cromossomo " + str(m.nome) + " - Aptidão: " +
+          str(m.heuristica()) + " - Encontrou: " + str(m.encontrou_saida) + " - Passos: " + str(len(m.passos)))
+    print("-> Melhor Global - Geracao: " + str(contador_geracoes) + " - Melhor: Cromossomo " + str(melhor.nome) + " - Aptidão: " +
+          str(melhor.heuristica()) + " - Encontrou: " + str(melhor.encontrou_saida) + " - Passos: " + str(len(melhor.passos)))
+
 def imprime_labirinto():
-    print("Labirinto: ")
+    logar("Labirinto: ")
 
     for i in range(tamanho_matriz):
-        print(str(labirinto[i]).replace("'", "").replace(",", ""))
+        logar(str(labirinto[i]).replace("'", "").replace(",", ""))
+
 
 def imprime_populacao(populacao):
-    for i in range(TAM_POPULACAO_INICIAL):        
-        print(populacao[i])
+    for i in range(TAM_POPULACAO_INICIAL):
+        logar(populacao[i])
+
 
 if __name__ == "__main__":
     main()
